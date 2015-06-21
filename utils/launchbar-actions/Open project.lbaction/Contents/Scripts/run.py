34cdd0417b3fc47e37cd6ac257df471fdebd8b33
Martin Duhem <martin.duhem@gmail.com>
2015-06-21 22:37:21 +0200

    `Open Project` action now uses vim

--- utils/launchbar-actions/Open project.lbaction/Contents/Scripts/run.py
+++ utils/launchbar-actions/Open project.lbaction/Contents/Scripts/run.py
@@ -2,4 +2,4 @@ import sys
 import subprocess
 import custom_env
 
-subprocess.call([custom_env.open_sublime_project, sys.argv[1]], shell = False)
+subprocess.call([custom_env.open_vim_project, sys.argv[1]], shell = False)
