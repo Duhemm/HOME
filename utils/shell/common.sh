e0137fc1c7b48fe17ea98d1b8e5552ab84c63431
Martin Duhem <martin.duhem@gmail.com>
2016-09-06 14:59:03 +0200

    Port some utils to Linux env

--- utils/shell/common.sh
+++ utils/shell/common.sh
@@ -11,7 +11,7 @@ projectfilename() {
 }
 
 has_terminal_running() {
-	running_terms=$(ps aux | grep Terminal | wc -l)
+	running_terms=$(ps aux | grep 'Terminal|konsole' | wc -l)
 	# There's always the process that corresponds to the grep, therefore
 	# we need to have at least two processes
 	if [[ $running_terms -gt 1 ]]; then
@@ -19,6 +19,26 @@ has_terminal_running() {
 	else
 		return 0
 	fi
+}
+
+linux_only() {
+    case $(uname -o) in
+        "GNU/Linux")
+            return 0
+            ;;
+        *)
+            caller=$funcstack[2]
+            echo "Function '$caller' should only be called from a Linux environment."
+            exit 1
+            ;;
+    esac
+}
+
+in_temp_file() {
+    linux_only
+    filename=$(tempfile)
+    echo "$*" > "$filename"
+    echo "$filename"
 }
 
 . "$HOME/.mini-env"
